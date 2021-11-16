1494e406359ec09257ad54401fcf3ea791cfefed
Barbara CANO <bcano@e2r9p8.clusters.42paris.fr>
2021-11-16 15:27:30 +0100

    send to github

---
+++ get_next_line.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 11:28:02 by bcano             #+#    #+#             */
+/*   Updated: 2020/06/02 16:57:14 by bcano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char				*ft_clean(char *save, char **line, int r)
+{
+	unsigned int	i;
+	char			*tmp;
+
+	i = 0;
+	while (save[i])
+	{
+		if (save[i] == '\n')
+			break ;
+		i++;
+	}
+	if (i < ft_strlen(save))
+	{
+		*line = ft_substr(save, 0, i);
+		tmp = ft_substr(save, i + 1, ft_strlen(save));
+		free(save);
+		save = ft_strdup(tmp);
+		free(tmp);
+	}
+	else if (r == 0)
+	{
+		*line = save;
+		save = NULL;
+	}
+	return (save);
+}
+
+char				*ft_save(char *buffer, char *save)
+{
+	char			*tmp;
+
+	if (save)
+	{
+		tmp = ft_strjoin(save, buffer);
+		free(save);
+		save = ft_strdup(tmp);
+		free(tmp);
+	}
+	else
+		save = ft_strdup(buffer);
+	return (save);
+}
+
+int					get_next_line(int fd, char **line)
+{
+	static char		*save[4096];
+	char			buff[BUFFER_SIZE + 1];
+	int				r;
+
+	if (BUFFER_SIZE == 0)
+		return (0);
+	while ((r = read(fd, buff, BUFFER_SIZE)))
+	{
+		if (r == -1)
+			return (r);
+		buff[r] = '\0';
+		save[fd] = ft_save(buff, save[fd]);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	if (r <= 0 && !save[fd])
+	{
+		*line = ft_strdup("");
+		return (r);
+	}
+	save[fd] = ft_clean(save[fd], line, r);
+	if (r <= 0 && !save[fd])
+		return (r);
+	return (1);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 11:28:44 by bcano             #+#    #+#             */
+/*   Updated: 2020/06/02 16:57:33 by bcano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t				ft_strlen(const char *s)
+{
+	size_t			i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char				*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	size_t			len_max;
+	size_t			i;
+	size_t			j;
+
+	if (!s)
+		return (NULL);
+	len_max = ft_strlen(s);
+	if (start >= len_max)
+		return (ft_strdup(""));
+	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	i = start;
+	j = 0;
+	while (j < len && s[i] != '\0')
+	{
+		str[j] = s[i];
+		j++;
+		i++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+char				*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	j;
+	int				len_2_str;
+
+	i = 0;
+	j = 0;
+	len_2_str = ft_strlen(s1) + ft_strlen(s2);
+	if (!s1 || !s2)
+		return (ft_strdup(""));
+	if (!(str = (char *)malloc(sizeof(char) * (len_2_str + 1))))
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char				*ft_strdup(const char *s1)
+{
+	char			*str;
+	unsigned int	i;
+	size_t			len_s1;
+
+	i = 0;
+	len_s1 = ft_strlen(s1);
+	str = (char*)malloc(len_s1 + 1);
+	if (str)
+	{
+		while (s1[i] != '\0' && i < len_s1)
+		{
+			str[i] = s1[i];
+			i++;
+		}
+		str[i] = '\0';
+	}
+	else
+		return (0);
+	return (str);
+}
+
+char				*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == '\0' && c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
